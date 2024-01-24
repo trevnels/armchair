5f17bcc972efb8e80b88b116c2507d64f90d62fc
trevnels <25140503+trevnels@users.noreply.github.com>
2024-01-24 16:40:34 -0500

    feat: use neon serverless driver

--- src/hooks/usedb.ts
+++ src/hooks/usedb.ts
@@ -1,5 +1,15 @@
-import {PrismaClient} from "@prisma/client"
+import { Pool, neonConfig } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { PrismaClient } from '@prisma/client'
+import ws from 'ws'
+
+neonConfig.webSocketConstructor = ws
+const connectionString = `${process.env.DATABASE_URL}`
+
+const pool = new Pool({connectionString: connectionString, connectionTimeoutMillis: 10000})
+const adapter = new PrismaNeon(pool)
+const prisma = new PrismaClient({adapter})
 
 export default function useDb() {
-    return new PrismaClient()
+    return prisma
 }
